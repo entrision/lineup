9d241413caa3c538793513c6c13745cfba548e68
Tim Gremore <timgremore@gmail.com>
2015-02-17 13:46:27 -0600

    Cleanup PlayerLineup and specs

--- app/models/player_lineup.rb
+++ app/models/player_lineup.rb
@@ -1,10 +1,12 @@
 class PlayerLineup < ActiveRecord::Base
+
   belongs_to :coach
+
   has_many   :players, through: :lineup_positions
   has_many   :lineup_positions, dependent: :destroy
 
   accepts_nested_attributes_for :lineup_positions, allow_destroy: true
 
-  validates_presence_of :title
+  validates :title, presence: true
 
 end
--- spec/models/player_lineup_spec.rb
+++ spec/models/player_lineup_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PlayerLineup, type: :model do
+
   subject(:lineup) { PlayerLineup.new }
 
-  context "when not valid" do
-    it "missing title" do
-      expect(lineup).to be_invalid
+  describe "#title" do
+    before do
+      lineup.title = nil
+      lineup.valid?
+    end
+
+    it 'assigns an error to title' do
+      expect(lineup.errors.has_key?(:title)).to be_truthy
     end
   end
 
