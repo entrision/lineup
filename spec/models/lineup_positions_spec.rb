29a9009e699f452814bb1ebbea31f2c5499fdcea
James T. Perreault <james@perreau.lt>
2015-03-04 09:45:13 -0500

    cleanup lineup_positions_spec

--- spec/models/lineup_positions_spec.rb
+++ spec/models/lineup_positions_spec.rb
@@ -8,69 +8,82 @@ RSpec.describe LineupPosition, type: :model do
   it "has no duplicate players"
   it "has no duplicate positions"
 
-  describe "position change" do
-    let!(:player1) { create(:player, win: 2, loss: 1) }
-    let!(:player2) { create(:player, win: 1, loss: 2) }
-    let!(:player3) { create(:player, win: 3, loss: 0) }
-    let!(:player4) { create(:player, win: 2, loss: 1) }
-    let!(:no_match_p1) { create(:player) }
-    let!(:no_match_p2) { create(:player) }
-    let!(:no_match_p3) { create(:player) }
-
+  describe "changing position" do
+    let!(:player1) { create(:player) }
+    let!(:player2) { create(:player) }
+    let!(:player3) { create(:player) }
     let!(:lineup1)  { create(:player_lineup) }
-    let!(:lineup2)  { create(:player_lineup) }
-
     let!(:position1) { create(:lineup_position, position: 1, player: player1, player_lineup: lineup1) }
     let!(:position2) { create(:lineup_position, position: 2, player: player2, player_lineup: lineup1) }
     let!(:position3) { create(:lineup_position, position: 3, player: player3, player_lineup: lineup1) }
-    let!(:position4) { create(:lineup_position, position: 4, player: player4, player_lineup: lineup1) }
-    let!(:no_match_pos1) { create(:lineup_position, position: 1, player: no_match_p1, player_lineup: lineup2) }
-    let!(:no_match_pos2) { create(:lineup_position, position: 2, player: no_match_p2, player_lineup: lineup2) }
-    let!(:no_match_pos3) { create(:lineup_position, position: 3, player: no_match_p3, player_lineup: lineup2) }
 
-    context "without playing matches" do
+    context "with no matches played" do
       it "moves without restriction" do
-        no_match_pos3.position = 1
-        expect(no_match_pos3).to be_valid
+        position3.position = 1
+        expect(position3).to be_valid
       end
     end
 
     context "with matches played" do
-
-      it "moves 1 spot" do
-        position3.player.win += 1
-        position3.position = 2
-        expect(position3).to be_valid
+      before do
+        player1.update_attributes(win: 2, loss: 1)
+        player2.update_attributes(win: 1, loss: 2)
+        player3.update_attributes(win: 3, loss: 0)
       end
 
-      it "cannot move 2 spots" do
-        position3.position = 1
-        expect(position3).to be_invalid
+      describe "having not played at current position" do
+        before do
+          position3.update_attributes(player_match_count_at_change: 3)
+          position3.position = 2
+          position3.valid?
+        end
+
+        it "assigns an error" do
+          expect(position3.errors.has_key?(:position)).to be_truthy
+        end
       end
 
-      it "must play a match before moving again" do
-        position2.position = 1
-        expect(position2).to be_invalid
-      end
+      describe "having played match(es) at current position" do
+        before do
+          position2.update_attributes(player_match_count_at_change: 2)
+          position3.update_attributes(player_match_count_at_change: 2)
+        end
 
-      it "player must have higher win % to overtake position" do
-        position4.position = 3
-        expect(position4).to be_invalid
+        context "moving up with higher win percentage" do
+          it "moves 1 spot" do
+            position3.position = 2
+            expect(position3).to be_valid
+          end
+
+          it "cannot move 2 spots" do
+            position3.position = 1
+            position3.valid?
+            expect(position3.errors.has_key?(:position)).to be_truthy
+          end
+        end
+
+        context "moving up with lower win percentage" do
+          before do
+            position2.position = 1
+            position2.valid?
+          end
+
+          it "assigns error to position" do
+            expect(position2.errors.has_key?(:position)).to be_truthy
+          end
+        end
       end
 
       context "after the first 5 matches" do
-        before {
-          # add 3 mathces for 6 total
-          position3.player.win += 2
-          position3.player.loss += 1
-          # set match count when position was taken
-          position3.player_match_count_at_change = 4
-          position3.save!
-        }
+        before do
+          player3.update_attributes(win: 5, loss: 1)
+          position3.update_attributes(player_match_count_at_change: 4)
+        end
 
         it "cannot move before 3 matches at new position" do
           position3.position = 2
-          expect(position3).to be_invalid
+          position3.valid?
+          expect(position3.errors.has_key?(:position)).to be_truthy
         end
 
         it "changes position after 3 matches" do
@@ -78,29 +91,22 @@ RSpec.describe LineupPosition, type: :model do
           position3.position = 2
           expect(position3).to be_valid
         end
-
       end
-
-
     end
   end
 
-  describe "mandatory move" do
+  describe "#check_and_move_mandatory" do
     let!(:player1) { create(:player, win: 3, loss: 5) }
     let!(:player2) { create(:player, win: 7, loss: 1) }
-
     let!(:lineup1)  { create(:player_lineup) }
-
     let!(:position1) { create(:lineup_position, position: 1, player: player1, player_lineup: lineup1, player_match_count_at_change: 2) }
     let!(:position2) { create(:lineup_position, position: 2, player: player2, player_lineup: lineup1, player_match_count_at_change: 2) }
 
     context "with 20% win difference" do
-      before {
-        position1.player_match_count_at_change = 2
-        position2.player_match_count_at_change = 2
-        position1.save
-        position2.save
-      }
+      before do
+        position1.update_attributes(player_match_count_at_change: 2)
+        position2.update_attributes(player_match_count_at_change: 2)
+      end
 
       it "automatically moves players" do
         position1.check_and_move_mandatory
@@ -111,6 +117,5 @@ RSpec.describe LineupPosition, type: :model do
         expect(position1.locked?).to eq(true)
       end
     end
-
   end
 end
